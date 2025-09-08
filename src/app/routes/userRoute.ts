f406dd78b0df050100afae6fc4e67ec04a2445fd
unknown <areyad203011@bscse.uiu.ac.bd>
2025-09-08 23:00:57 +0600

    validation middleware added in the route

--- src/app/routes/userRoute.ts
+++ src/app/routes/userRoute.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { userControllers } from "../controllers/registerUserController";
+import { validate } from "../middlewares/validateMiddleware";
+import { userValidation } from "../validators/userValidation";
 
 const userRoutes = Router();
 
-userRoutes.post("/register", userControllers.registerUser);
+userRoutes.post(
+  "/register",
+  validate(userValidation),
+  userControllers.registerUser
+);
 
 export default userRoutes;
