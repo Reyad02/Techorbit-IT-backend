1b0abf677268ba39e25958c4c813d990718cb72f
unknown <areyad203011@bscse.uiu.ac.bd>
2025-09-08 20:49:27 +0600

    config all the env variable

---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  mongooose_uri: process.env.MONGOOSE_URL,
+  port: process.env.PORT,
+  saltRounds: process.env.SALT_ROUNDS,
+  jwt_secret: process.env.JWT_SECRET,
+  jwt_expiration: process.env.JWT_EXPIRED,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app";
-import dotenv from "dotenv";
-import path from "path";
+import config from "./app/config";
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const main = async () => {
   try {
-    await mongoose.connect(process.env.MONGOOSE_URL as string);
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
+    await mongoose.connect(config.mongooose_uri as string);
+    app.listen(config.port, () => {
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (err: any) {
     console.log(err);
