7837ed3f2bfc37d7cf07f658b898cbc80b65babb
unknown <areyad203011@bscse.uiu.ac.bd>
2025-09-08 20:46:26 +0600

    database and server setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi, your project is working!!!");
+});
+
+export default app;
